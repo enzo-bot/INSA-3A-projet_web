8ff049e2f3ba3c3bee6ef8b2692bef5477be67da
tensaiji <47110246+tensaiji@users.noreply.github.com>
2022-12-07 02:16:10 +0100

    propre

--- game.js
+++ game.js
@@ -1,40 +1,64 @@
-var jeu = null;
 const maxTries = 6;
+
+var active = false;
+
+var game = null;
+var keyboard = null;
+var popup = null;
 
 var word = ""
 var wordSize = 0;
-var input = [];
-var caseStatus = [];
+var input = "";
 var rows = [];
 var nRow = 0;
 var nLetter = 0;
 
 function checkRow()
 {
+    nLetter = 0;
 
+    if (String(input) === word)
+    {
+        for (let i = 0; i < wordSize; i++)
+            rows[nRow].childNodes[i].classList.add("victory");
+        finish(true);
+    }
+    else
+    {
+        const last = wordSize - 1;
+        for (let i = 0; i < wordSize; i++)
+        {
+            if (input[i] === word[i])
+                rows[nRow].childNodes[i].classList.add("correct");
+            else
+                rows[nRow].childNodes[i].classList.add("wrong");
+        }
+        nRow++;
+        if (nRow == maxTries)
+            finish(false);
+        else input = "";
+    }
 }
 
 function addLetter(letter)
 {
-    input.push(letter);
-    updateCase();
-    if (nLetter == wordSize - 1)
+    if (nLetter < wordSize)
     {
-        nLetter = 0;
-        nRow++;
-        checkRow();
-        input = [];
-        if (nRow == maxTries)
-            newGame();
+        input += letter.toLowerCase();
+        rows[nRow].childNodes[nLetter].innerHTML = input[nLetter];
+        if (nLetter == wordSize - 1) checkRow();
+        else nLetter++;
     }
-    else nLetter++;
 }
 
 function removeLetter()
 {
-    if (nLetter > 0) nLetter--;
-    input[nLetter] = "";
-    updateCase();
+    if (nLetter > 0)
+    {
+        nLetter--;
+        input[nLetter] = "";
+        rows[nRow].childNodes[nLetter].innerHTML = "";
+    }
 }
 
 function generateWord()
@@ -42,42 +66,83 @@ function generateWord()
     return "TABLEAU";
 }
 
-function updateCase()
-{
-    rows[nRow].childNodes[nLetter].textContent = input[nLetter];
-}
-
 function generateGame()
 {
-    while (jeu.firstChild) jeu.removeChild(jeu.firstChild);
+    while (game.firstChild) game.removeChild(game.firstChild);
     for (let i = 0; i < maxTries; i++)
     {
         let row = document.createElement("div");
         row.classList.add("row");
         rows.push(row);
-        jeu.appendChild(row);
+        game.appendChild(row);
         for (let j = 0; j < wordSize; j++)
         {
             let c = document.createElement("div");
-            c.classList.add("case");
+            c.classList.add("cell");
             row.appendChild(c);
         }
     }
 }
 
-addEventListener('keydown', (e) => {
-    addLetter(String.fromCharCode(e.keyCode));
+function generateKeyboard()
+{
+    const keys = keyboard.querySelectorAll("td");
+    if (!!keys)
+        keys.forEach(key => {
+            if (key.childElementCount === 0)
+                key.onclick = (e) => addLetter(key.innerHTML);
+            else if (key.id === "delete")
+                key.onclick = (e) => removeLetter();
+            else if (key.id === "enter")
+                key.onclick = (e) => checkRow();
+        });
+}
+
+const letterRegex = /[a-z]/i;
+
+document.addEventListener('keydown', (e) => {
+    if (e.altKey || e.ctrlKey) return;
+    if (active)
+    {
+        if(e.key.length === 1 && e.key.match(letterRegex)) addLetter(e.key);
+        else if(e.key === "Backspace") removeLetter();
+        else if(e.key === "Enter") checkRow();
+    }
+    else if (e.key === "Enter") restart();
 });
 
-function newGame()
+function play(gameId, keyboardId, popupId)
 {
-    word = generateWord();
-    wordSize = word.length;
-    input = [];
-    caseStatus = [];
-    rows = [];
-    nRow = 0;
-    nLetter = 0;
+    game = document.getElementById(gameId);
+    keyboard = document.getElementById(keyboardId);
+    popup = document.getElementById(popupId);
+    if (!!game && !!keyboard && !!popup)
+    {
+        word = generateWord().toLowerCase();
+        wordSize = word.length;
+        input = "";
+        rows = [];
+        nRow = 0;
+        nLetter = 0;
 
-    generateGame();
+        generateGame();
+        active = true;
+
+        generateKeyboard();
+    }
+}
+
+function finish(victory)
+{
+    active = false;
+    popup.querySelector("#message").innerHTML = victory ? "Gagné !" : "Perdu...";
+    popup.querySelector("#mot>b").innerHTML = word.toUpperCase();
+    popup.classList.add("active");
+    popup.onclick = () => restart();
+}
+
+function restart()
+{
+    popup.classList.remove("active");
+    play(game.id, keyboard.id, popup.id);
 }
